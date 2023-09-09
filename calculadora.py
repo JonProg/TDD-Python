8f2ec03848272a9ccab5f943d317aafef7853c18
jon_prog <jonashenri.aquino@gmail.com>
2023-09-08 22:04:04 -0300

    add asserts in function soma

--- calculadora.py
+++ calculadora.py
@@ -1,2 +1,4 @@
 def soma(x,y):
+    assert isinstance(x, (int, float)), 'x tem que ser um valor int ou float'
+    assert isinstance(y, (int, float)), 'y tem que ser um valor int ou float'
     return x+y
